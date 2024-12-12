89a1b2e45953ee0ceca991737c0aae97f78464e5
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-12-12 09:56:01 +0530

    changes

---
+++ waiver-react/src/generated templates/template.js
@@ -0,0 +1,35 @@
+[
+    {
+      "id": "1733977369964",
+      "type": "TextField",
+      "props": {
+        "label": "interesting"
+      }
+    },
+    {
+      "id": "1733977388397",
+      "type": "TextField",
+      "props": {
+        "label": "Text Field"
+      }
+    },
+    {
+      "id": "1733977395131",
+      "type": "Checkbox",
+      "props": {
+        "label": "Checkbox"
+      }
+    },
+    {
+      "id": "1733977398730",
+      "type": "Dropdown",
+      "props": {
+        "label": "Dropdown",
+        "options": [
+          "hey",
+          "hello",
+          "have"
+        ]
+      }
+    }
+  ]
